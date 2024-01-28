f966ebc7ed562fd4fa7d63f7ec5899659e61f495
skkk <shengkuan@shanghaitech.edu.cn>
2024-01-29 01:22:57 +0800

    args parser

--- src/main.go
+++ src/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+    "fmt"
+    "io"
+    "net/http"
+)
+
+import "shengkuan/mini_spider/utils"
+
+var (
+	displayVersion bool
+	confFile       string
+	logDir         string
+)
+
+func init() {
+    flag.Usage = utils.DisplayHelpMenu
+	flag.BoolVar(&displayVersion, "v", false, "display spider version then exit")
+	flag.StringVar(&confFile, "c", "../conf/spider.conf", "set config file path")
+	flag.StringVar(&logDir, "l", "../log/", "set log directory")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+    flag.Parse()
+    if displayVersion {
+        fmt.Println("mSpider version: 1.0.0")
+        return
+    }
+    fmt.Println("confFile:", confFile)
+    fmt.Println("logDir:", logDir)
+
+    resp, err := http.Get("http://www.baidu.com/")
+    if err != nil {
+        fmt.Println("http get error", err)
+        return
+    }
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("read error", err)
+        return
+    }
+    fmt.Println(string(body))
 }
