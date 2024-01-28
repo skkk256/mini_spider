fcce7a2b4027e357a0a03f6875f7104bda7bb837
skkk <shengkuan@shanghaitech.edu.cn>
2024-01-28 20:44:18 +0800

    framework and config parser

---
+++ src/config/config.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"errors"
+	"path/filepath"
+)
+
+import (
+	"github.com/go-gcfg/gcfg"
+)
+
+import (
+	"shengkuan/mini_spider/utils"
+)
+
+type Config struct {
+	Spider struct {
+		UrlListFile     string // 种子文件路径
+		OutputDirectory string // 下载目录
+		MaxDepth        int    // 最大抓取深度
+		CrawlInterval   int    // 抓取间隔
+		CrawlTimeout    int    // 抓取超时
+		TargetUrl       string // 目标文件正则
+		ThreadCount     int    // 抓取routine数
+	}
+}
+
+func (c *Config) Check() (bool, error) {
+	if !util.IsFileExist(c.Spider.UrlListFile) {
+		return false, errors.New("UrlListFile not exist: " + c.Spider.UrlListFile)
+	}
+	if !util.IsDirExist(c.Spider.OutputDirectory) {
+		return false, errors.New("OutputDirectory not exist: " + c.Spider.OutputDirectory)
+	}
+	if c.Spider.CrawlInterval < 0 {
+		return false, errors.New("CrawlInterval must greater than zero")
+	}
+	if c.Spider.CrawlTimeout <= 0 {
+		return false, errors.New("CrawlTimeout must greater than zero")
+	}
+	if c.Spider.TargetUrl == "" {
+		return false, errors.New("TargetUrl empty")
+	}
+	if c.Spider.ThreadCount <= 0 {
+		return false, errors.New("ThreadCount must greater than zero")
+	}
+	return true, nil
+}
+
+func LoadConfigFromFile(filePath string) (*Config, error) {
+	var conf Config
+	err := gcfg.ReadFileInto(&conf, filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	configDir := filepath.Dir(filePath)
+	conf.Spider.UrlListFile = resolvePath(configDir, conf.Spider.UrlListFile)
+	conf.Spider.OutputDirectory = resolvePath(configDir, conf.Spider.OutputDirectory)
+
+	if _, err := conf.Check(); err != nil {
+		return nil, err
+	}
+
+	return &conf, nil
+}
+
+func resolvePath(basePath, targetPath string) string {
+	if filepath.IsAbs(targetPath) {
+		return targetPath
+	}
+	return filepath.Join(basePath, targetPath)
+}
---
+++ src/go.mod
@@ -0,0 +1,16 @@
+module shengkuan/mini_spider
+
+go 1.21.6
+
+require (
+	github.com/go-gcfg/gcfg v1.2.3
+	github.com/jeanphorn/log4go v0.0.0-20231225120528-d93eb9001e51
+	golang.org/x/net v0.20.0
+)
+
+require (
+	github.com/toolkits/file v0.0.0-20160325033739-a5b3c5147e07 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/gcfg.v1 v1.2.3 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+)
